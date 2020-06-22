079c3590756b2ce5e8a73055998f26184d495727
Bruno Salerno <br.salerno@gmail.com>
2020-06-22 16:49:19 -0300

    Add example
    Small fix

---
+++ example/README.md
@@ -0,0 +1,15 @@
+### Cut line mode example
+
+- Install dependencies:
+```
+npm install
+```
+
+- Run `npm run serve` in this folder.
+
+- Access the example at http://192.168.0.9:8080.
+
+#### Build
+```
+npm run build
+```
---
+++ example/index.js
@@ -0,0 +1,36 @@
+import mapboxgl from 'mapbox-gl';
+import MapboxDraw from '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw';
+import CutLineMode from 'mapbox-gl-draw-cut-line-mode';
+
+mapboxgl.accessToken = '';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/satellite-v9',
+  center: [-58.381944,-34.599722],
+  zoom: 13
+});
+
+var modes = MapboxDraw.modes;
+modes.cut_line = CutLineMode;
+
+window.draw = new MapboxDraw({
+  displayControlsDefault: false,
+  controls: {
+    line_string: true,
+  },
+  modes: modes
+});
+
+map.addControl(window.draw);
+
+map.on('load', function(){
+  map.on('draw.create', handleEvent);
+  map.on('draw.delete', handleEvent);
+
+  function handleEvent(e) {
+    console.log(e);
+    if (e.type == 'draw.delete') {
+      map.getCanvas().style.cursor = 'inherit';
+    }
+  }
+});
